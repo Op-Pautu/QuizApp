c1511f2d8b9af45bb2690849756b8b043f1d644c
Op-Pautu <pautuop@gmail.com>
2023-07-09 14:22:54 +0530

    Add basic style to site

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizzes</h1>
+      <input type="text" placeholder="Search...">
+    </header>
+    <div class="options-container">
+      <div class="card">
+        <img src="https://img.jagranjosh.com/images/2023/January/2512023/Math-1_11zon.jpg" alt="">
+        <div class="card-text">
+          <h2>Math</h2>
+          <p>15 Questions</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    margin: 0 auto;
+    max-width: 1000px;
+  }
+  header {
+    margin-bottom: 10px;
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+
+  }
+  header h1 {
+    font-weight: bold;
+    margin-right: 30px;
+  }
+
+  header input {
+    border: none;
+    background-color: rgba(128, 128, 128, 0.1);
+    padding: 10px;
+    border-radius: 5px;
+  } 
+  .options-container {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 40px;
+  }
+
+  /* CARD */
+  .card {
+    width: 310px;
+    overflow: hidden;
+    border-radius: 2%;
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+    margin-bottom: 35px;
+    margin-right: 20px;
+    cursor: pointer;
+  }
+  .card img {
+    width: 100%;
+    height: 190px;
+    margin: 0;
+  }
+  .card .card-text {
+    padding: 0 5px;
+  }
+  .card .card-text h2 {
+    font-weight: bold;
+  }
+</style>
