e3a0eb3cb46366f073de9ce27f2d24ee862e1240
Op-Pautu <pautuop@gmail.com>
2023-07-10 21:36:44 +0530

    Write the logic for the different quizzes

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import QuizzesView from "../views/QuizzesView.vue"
+import QuizView from "../views/QuizView.vue"
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes : [
+        {
+        path: '/',
+        name: "Quizzes",
+        component: QuizzesView
+        },
+        {
+            path: '/quiz/:id',
+            name: "Quiz",
+            component: QuizView
+        }
+]
+})
+
+export default router
