9657ab414aaa1d3ce34c788ad6b1787fcaba5af2
Op-Pautu <pautuop@gmail.com>
2023-07-10 22:02:24 +0530

    Sync the correct answers with the UI

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,14 +1,22 @@
 <script setup>
-    import { defineProps } from 'vue';
+    import { defineProps, defineEmits } from 'vue';
+    const emit = defineEmits(['selectOption'])
     const {question} = defineProps(['question'])
+    const emitSelectedOption = (isCorrect) => {
+        emit('selectOption', isCorrect)
+    }
 </script>
 <template>
      <div class="question-container">
             <h1 class="question">{{ question.text }}</h1>
         </div>
         <div class="options-container">
-            <div v-for="option in question.options" :key="option.id" class="option">
-                <p class="option-label">{{ option.label }}</p>
+            <div 
+            v-for="option in question.options" 
+            :key="option.id" 
+            class="option"
+            @click="emitSelectedOption(option.isCorrect)">
+            <p class="option-label">{{ option.label }}</p>
                     <div class="option-value">
                     <p>{{ option.text }} </p>
                     </div>
